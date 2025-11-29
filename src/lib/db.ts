ca079a2f1cbc57ebce5838b895e2c906a3277f8e
Fujita <y.fujtia@toyosteel.jp>
2025-11-30 01:04:43 +0900

    Fix db.ts to use environment variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_emUFP8HfOa2Z@ep-noisy-boat-a1bcbu4t-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
